02c397fde1609d54f6d8c2530f89ff74ebabcac8
Orkhand4 <orkhand4@mail.ru>
2024-12-20 05:00:22 +0400

    hero updated

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import arrowRight from "../assets/icons/global/Arrow Right.svg";
 import heroImg from "../assets/images/Hero Image.svg";
+import burst from "../assets/images/Burst-pucker.svg";
+import ellipse from "../assets/images/Ellipse.svg";
 
 const Hero = () => {
   return (
-    <div className="bg-neutralWhite-100 ">
-      <div className="container flex items-center justify-between">
+    <div className="bg-neutralWhite-100">
+      <div className="container flex items-center justify-between relative">
         <div>
           <div className="flex flex-col">
-            <h2 className="font-semibold text-3xl text-neutralBlack-800">
+            <h2 className="font-semibold text-4xl text-neutralBlack-800">
               Fresh Arrivals Online
             </h2>
-            <p className="text-neutralBlack-600 mt-4 text-xs">
-              Discover Our Newest Collection Today
+            <p className="text-neutralBlack-600 mt-4 text-base">
+              Discover Our Newest Collection Today.
             </p>
           </div>
           <button className="mt-12 rounded px-6 py-3 bg-neutralBlack-900 text-neutralWhite-900">
@@ -20,13 +22,15 @@ const Hero = () => {
             <img
               src={arrowRight}
               alt="Arrow Right Icon"
-              className="text-neutralWhite-900 inline-block"
+              className="pl-1 inline-block"
             />
           </button>
         </div>
-        <div>
-          <img src={heroImg} alt="" />
+        <div className="z-10">
+          <img src={heroImg} alt="Hero Image" />
         </div>
+        <img src={burst} alt="burst-pucker" className="absolute top-8 left-2/3 z-0" />
+        <img src={ellipse} alt="ellipse" className="absolute left-2/3 bottom-0 z-0" />
       </div>
     </div>
   );
