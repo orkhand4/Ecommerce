30fe8669cc32e40d7301cfad884f603355575282
Orkhand4 <orkhand4@mail.ru>
2024-12-22 06:00:04 +0400

    Home Page updated

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/header'
 import NotificationBar from '../components/notificationBar'
+import Footer from '../components/footer'
 
 const AboutPage = () => {
   return (
@@ -8,6 +9,7 @@ const AboutPage = () => {
       <NotificationBar/>
       <Header/>
       <h1 className='text-6xl py-28 text-center'>ABOUT PAGE</h1>
+      <Footer/>
     </div>
   )
 }
--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/header'
 import NotificationBar from '../components/notificationBar'
+import Footer from '../components/footer'
 
 const ContactPage = () => {
   return (
@@ -8,6 +9,7 @@ const ContactPage = () => {
       <NotificationBar/>
       <Header/>
       <h1 className='text-6xl py-28 text-center'>CONTACT PAGE</h1>
+      <Footer/>
     </div>
   )
 }
